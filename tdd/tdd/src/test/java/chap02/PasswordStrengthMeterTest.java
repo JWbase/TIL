31a5b7e242935708920b0fb7c17f89d6db2e39db
LEEJUNGWOO <jungwoo1002@gmail.com>
2023-12-12 23:01:56 +0900

    feat: 암호 검사기

---
+++ tdd/tdd/src/test/java/chap02/PasswordStrengthMeterTest.java
@@ -0,0 +1,67 @@
+package chap02;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class PasswordStrengthMeterTest {
+
+    private PasswordStrengthMeter meter = new PasswordStrengthMeter();
+
+    private void assertStrength(String password, PasswordStrength expStr) {
+        PasswordStrength result = meter.meter(password);
+        assertThat(result).isEqualTo(expStr);
+    }
+
+    @Test
+    void meetsAllCriteria_Then_Strong() {
+        assertStrength("ab12!@AB", PasswordStrength.STRONG);
+        assertStrength("abc1!Add", PasswordStrength.STRONG);
+    }
+
+    @Test
+    void meetsOtherCriteria_except_for_Length_Then_Normal() {
+        assertStrength("ab12!@A", PasswordStrength.NORMAL);
+        assertStrength("Ab12!c", PasswordStrength.NORMAL);
+    }
+
+    @Test
+    void meetsOtherCriteria_except_for_number_Then_Normal() {
+        assertStrength("ab!@ABqwer", PasswordStrength.NORMAL);
+    }
+
+    @Test
+    void nullInput_Then_Invalid() {
+        assertStrength(null, PasswordStrength.INVALID);
+    }
+
+    @Test
+    void emptyInput_Then_Invalid() {
+        assertStrength("", PasswordStrength.INVALID);
+    }
+
+    @Test
+    void meetsOtherCriteria_except_for_Uppercase_Then_Normal() {
+        assertStrength("ab12!adf", PasswordStrength.NORMAL);
+    }
+
+    @Test
+    void meetsOnlyLengthCriteria_Then_Weak() {
+        assertStrength("abcdefghi", PasswordStrength.WEAK);
+    }
+
+    @Test
+    void meetsOnlyNumCriteria_Then_Weak() {
+        assertStrength("12345", PasswordStrength.WEAK);
+    }
+
+    @Test
+    void meetsOnlyUpperCriteria_Then_Weak() {
+        assertStrength("ABZEF", PasswordStrength.WEAK);
+    }
+
+    @Test
+    void meetsNoCriteria_Then_Weak() {
+        assertStrength("abc", PasswordStrength.WEAK);
+    }
+}
